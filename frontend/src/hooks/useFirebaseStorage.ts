bca4bc133ba4f146088b7d066b3cb6d70718fa20
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2025-03-15 18:09:50 -0300

    fix: ajustando storage

--- frontend/src/hooks/useFirebaseStorage.ts
+++ frontend/src/hooks/useFirebaseStorage.ts
@@ -3,9 +3,11 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 export function useFirebaseStorage() {
   async function uploadFile(file: File): Promise<string | null> {
+    const randomName = Math.random().toString(36).substring(7);
     try {
       const storage = getStorage(app);
-      const storageRef = ref(storage, `pictures/${file.name}`);
+      console.log(storage)
+      const storageRef = ref(storage, `pictures/${randomName}-${file.name}`);
       await uploadBytes(storageRef, file);
       const url = await getDownloadURL(storageRef)
       return url;
